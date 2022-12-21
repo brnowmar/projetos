5b9e19dee84187d5e9970686a361e005a16b46c2
brnowmar <bruno.mar.rj@gmail.com>
2022-12-21 00:16:08 -0300

    desafios5

---
+++ desafios/desafio5.js
@@ -0,0 +1,16 @@
+let newVar = [7, 4, 8, 1, 5]
+
+function myFunction (arg) {
+    return arg;
+}
+
+
+console.log(myFunction(newVar));    
+console.log(myFunction(newVar)[1]);
+
+function myFunction2(arr, number){
+    let number;
+    return arr[0]
+};
+
+console.log(myFunction2(newVar, 999))
